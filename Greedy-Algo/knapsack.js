09dbec3d1b4cabfb74b189510feb7213b622b0d4
caixiaocong <caistrong@outlook.com>
2017-12-10 02:07:39 +0800

    finish Greedy-Algo example knapsack

---
+++ Greedy-Algo/knapsack.js
@@ -0,0 +1,55 @@
+//背包问题 的贪心选择算法
+//
+//0-1背包问题每个物品只有放和不放两种选择（可用动态规划方法求解）
+//背包问题则可以放入一部分（可用贪心选择求解）
+
+class Diamond {
+    constructor(name,weight,value){
+        this.name = name
+        this.weight = weight
+        this.value = value
+    }
+}
+
+var diamonds=new Array(3)
+diamonds[0]=new Diamond("大米",5,20)
+diamonds[1]=new Diamond("小麦",4,10)
+diamonds[2]=new Diamond("面粉",3,12)
+
+function knapsackGreedy(diamondArr,c){
+    if(!diamondArr instanceof Array) return
+    if(!c) return
+
+    let n = diamondArr.length
+
+    diamondArr.forEach((element,index,array) => {
+        Object.defineProperty(element,'valuePerWeight',{value:element.value/element.weight})
+    });
+
+    let sortedArr = diamondArr.sort((a,b)=>{
+        if(a.valuePerWeight>b.valuePerWeight) return -1
+        if(a.valuePerWeight<b.valuePerWeight) return 1
+        return 0
+    })
+
+    let tempc = c,
+        sumValue = 0,
+        i=0
+    while(tempc){
+        if(tempc>sortedArr[i].weight){
+            console.log("拿走了"+sortedArr[i].name)
+            sumValue+=sortedArr[i].value
+            tempc-=sortedArr[i].weight
+        }else{
+            console.log("拿走了"+tempc/sortedArr[i].weight+sortedArr[i].name)
+            sumValue+=(sortedArr[i].valuePerWeight)*tempc
+            tempc = 0
+        }
+        i++
+    }
+    
+    console.log("总价值"+sumValue)
+    
+}
+
+knapsackGreedy(diamonds,10)
