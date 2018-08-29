35e434dba9b6087e44db5a460026c1b227b7b9c8
caixiaocong <caistrong@outlook.com>
2018-08-29 16:31:42 +0800

    fix: travellingSalesmanProblem.js

--- Back-Tracking/travellingSalesmanProblem.js
+++ Back-Tracking/travellingSalesmanProblem.js
@@ -2,6 +2,12 @@
 
 //有Bug 不能得到正确的结果 第55行是有逻辑问题的
 
+function swap(arr,i,j){
+    var tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+}
+
 //Test Case
 var citys=[0,1,2,3]
 //我们的城市 0 1 2 3 
@@ -17,8 +23,7 @@ var distance2d = [[0 ,30,6 ,4 ],
 //   3| 6  5  0 20 
 //   4| 4 10 20  0 
 
-var minCost = 10000
-var bestPath = new Array()
+
 
 function tsp (citysArr,distance2dArr,cityStartIndex){
     //参数检测
@@ -28,23 +33,26 @@ function tsp (citysArr,distance2dArr,cityStartIndex){
 
     var citysCount = citys.length
 
+    var minCost = 10000
+    var bestPath = new Array()
+
     var curPath = new Array()
     var curCost = 0
     
-    curPath.push(citysArr[cityStartIndex]+1)
+    curPath.push.apply(curPath,[...citysArr,citysArr[cityStartIndex]])
     
-    DSFbackTracking(0)
+    DSFbackTracking(1)
 
-    console.log("minCost"+minCost)
+    console.log("minCost "+minCost)
 
     function DSFbackTracking(level){
-        var deepestLevel = citysCount-1
+        var deepestLevel = citysCount
         //到达最底层了，符合这个条件就是找到了一条通路（能回到起点）
-        if(level==deepestLevel && distance2dArr[level][cityStartIndex]>0){
+        if(level==deepestLevel && distance2dArr[level-1][cityStartIndex]>0){
             //回到起点
-            curCost += distance2dArr[level][cityStartIndex]
-            curPath.push(citys[cityStartIndex]+1)
+            curCost += distance2dArr[level-1][cityStartIndex]
             console.log(curPath)
+            console.log(curCost)
             if(curCost<minCost){
                 bestPath = curPath
                 minCost = curCost
@@ -53,16 +61,17 @@ function tsp (citysArr,distance2dArr,cityStartIndex){
         }
         else{
             for(let i=level;i<citysCount;i++ ){
-                //distance2d > 0 意味着cityStart和遍历的到这个城市有通路
-                //即是level的子树 citysArr[i]
-                if(distance2dArr[level][citysArr[i]]>0){
-                    curPath.push(citysArr[i]+1)
-                    curCost += distance2dArr[level][citysArr[i]]
+                //这里的level-1最初指向起点，i则是遍历除起点外的其他剩下的城市
+                //前一个判断条件是他们之间存在通路，后一个条件是剪枝函数
+                //每一次DSFbackTracking的目的在于确定该level应该去哪个城市，i是候选
+                if(distance2dArr[level-1][i]>0 && curCost < minCost){
+                    swap(curPath,level,i)
+                    curCost += distance2dArr[level-1][i]
                     //往深一层去找
                     DSFbackTracking(level+1)
                     //如果回溯回来
-                    curPath.pop()
-                    curCost -= distance2dArr[level][citysArr[i]]
+                    swap(curPath,level,i)
+                    curCost -= distance2dArr[level-1][i]
         
                 }
             }
